f7f31fffc44d380612756f1b5464ffbf43a556c9
Dellap <Del.251955@gmail.com>
2019-11-28 11:47:44 +0700

    create git

---
+++ app/src/main/java/com/example/banggiav2/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.banggiav2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+import android.text.InputType
+
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
